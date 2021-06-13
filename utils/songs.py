8875e6b95a0ff1d21bcf0f284386fb8e4d5f9d56
ItsArtemiz <itsartemiz01@gmail.com>
2021-06-14 00:06:09 +0530

    Delete Previous Stuff

--- utils/songs.py
+++
@@ -1,8 +0,0 @@
-songs = [
-    "Late Night Blues by Hriday (feat. Ayaz Hamid)",
-    "Prophylaxis by ROBBER",
-    "Happy by BENZOD",
-    "Paranormal by JG Keeper",
-    "Cassete by Apratim",
-    "Mood Swings by BENZOD"
-]
