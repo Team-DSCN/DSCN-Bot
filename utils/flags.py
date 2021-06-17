c29382b9c022c16313f6a6f5306e14d66c9dd623
ItsArtemiz <itsartemiz01@gmail.com>
2021-06-17 22:50:58 +0530

    Added ArtistEdit

--- utils/flags.py
+++ utils/flags.py
@@ -10,4 +10,9 @@ class ArtistAdd(commands.FlagConverter, case_insensitive=True):
     music: str = commands.flag(name='music', aliases=['style'])
     playlist: str = commands.flag(name='playlist', aliases=['list'])
     avatar: Optional[Union[discord.User, str]] = commands.flag(default=lambda ctx: ctx.guild.icon.url)
-    
+
+class ArtistEdit(commands.FlagConverter, case_insensitive=True):
+    name: Optional[str]
+    music: Optional[str]
+    playlist: Optional[str]
+    avatar: Optional[Union[discord.User, str]] = commands.flag(default=lambda ctx: ctx.guild.icon.url)
