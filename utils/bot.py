31b000c9f8162057a79117f5487da7ad252094a6
ItsArtemiz <itsartemiz01@gmail.com>
2021-06-20 02:18:42 +0530

    fixed fetching prefixes

--- utils/bot.py
+++ utils/bot.py
@@ -57,12 +57,11 @@ async def get_pre(bot, message:discord.Message) -> Iterable[str]:
     if message.guild is None:
         base.append('.')
     else:
-        try:
-            prefixes:List = (await bot.utils.find_one({'name':'prefixes'}))[message.guild.id]
-        except (TypeError, KeyError, AttributeError):
-            base.append('.')
+        settings: dict = await bot.utils.find_one({'guildId':message.guild.id})
+        if settings:
+            base.extend(settings.get('prefixes', []))
         else:
-            base.extend(prefixes)
+            base.append('.')
     return base
 
 class Bot(commands.Bot):
@@ -102,6 +101,8 @@ class Bot(commands.Bot):
             self.session = aiohttp.ClientSession()
         if not hasattr(self, 'artists'):
             self.artists = Client(URI, 'DSCN', 'Artists')
+        if not hasattr(self, 'utils'):
+            self.utils = Client(URI, 'Utils', 'Settings')
         # if not hasattr(self, 'tags'):
         #     self.tags = Client(URI, 'DSCN', 'Tags')
         # if not hasattr(self, 'utils'):
