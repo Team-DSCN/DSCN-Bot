9b164577723dededd12fd31ca65875aab3f854eb
ItsArtemiz <itsartemiz01@gmail.com>
2021-06-21 17:59:49 +0530

    Added botchannel

--- utils/checks.py
+++ utils/checks.py
@@ -1,6 +1,20 @@
 
+from utils.errors import NotBotChannel
+from utils.context import Context
 from discord.ext import commands
 
 admin = commands.has_role(781796816257548308)
 staff = commands.has_role(850669530661388288)
 ar = commands.has_role(820579878826278932)
+
+def botchannel():
+    async def predicate(ctx: Context):
+        if ctx.guild is None:
+            raise commands.NoPrivateMessage('This command cannot be used in DMs.')
+        settings = await ctx.bot.utils.find_one({'guildId':ctx.guild.id})
+        if settings is None:
+            return True
+        if ctx.channel.id in settings.get('disabledChannels', []):
+            raise NotBotChannel(f'Cannot use bot commands in {ctx.channel} since it is not whitelisted.')
+        return True
+    return commands.check(predicate)
