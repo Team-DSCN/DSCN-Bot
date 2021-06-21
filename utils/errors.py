a3783ad88a88d2ac536b352e31403b8c47b7d09e
ItsArtemiz <itsartemiz01@gmail.com>
2021-06-21 17:56:09 +0530

    Added NotBotChannel

--- utils/errors.py
+++ utils/errors.py
@@ -1,2 +1,16 @@
+from discord.ext import commands
+
+
 class CollectionNotSet(Exception):
     pass
+
+class NotBotChannel(commands.CheckFailure):
+    def __init__(self, message=None, *args):
+        if not message:
+            message = (
+                'The current channel is not whitelisted.'\
+                ' Use the commands in a channel that is whitelisted.'
+            )
+        else:
+            message = message
+        super().__init__(message=message, *args)
